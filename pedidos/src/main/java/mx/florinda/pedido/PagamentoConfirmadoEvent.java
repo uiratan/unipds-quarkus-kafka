d54113af7436cd1eb478f42e11bc77cc1248769a
Uiratan Cavalcante <uiratan@gmail.com>
2025-09-25 02:24:10 -0300

    kafka consumer for pagamentosConfirmados

---
+++ pedidos/src/main/java/mx/florinda/pedido/PagamentoConfirmadoEvent.java
@@ -0,0 +1,14 @@
+package mx.florinda.pedido;
+
+public class PagamentoConfirmadoEvent {
+    public Long pagamentoId;
+    public Long pedidoId;
+
+    @Override
+    public String toString() {
+        return "PagamentoConfirmadoEvent{" +
+                "pagamentoId=" + pagamentoId +
+                ", pedidoId=" + pedidoId +
+                '}';
+    }
+}
